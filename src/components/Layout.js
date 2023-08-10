d8968caf1973836e87b9c2889fd2392df69983e0
hyomin <hm0704vip@incross.com>
2023-08-10 16:52:04 +0900

    tailwindcss 설치 및 layout 추가 중

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Layout = (props) => {
+  return (
+    <div className="grid min-h-screen grid-rows-header bg-zinc-100">
+      <div>Navbar</div>
+      <div className="grid md:grid-cols-sidebar">
+        <div>Sidebar</div>
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
