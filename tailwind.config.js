94697792c2ecedcb9e0962b4c85fbc95028642f8
Hm-source <swo8852@gmail.com>
2023-08-12 19:58:35 +0900

    tailwind, postcss config 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        sidebar: "300px auto", // 👈 for sidebar layout. adds grid-cols-sidebar class
-      }, 
-      gridTemplateRows: {
-        header: "64px auto", // 👈 for the navbar layout. adds grid-rows-header class
-      },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
     },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
-  plugins: [],
+  // plugins: [
+  //   require('@tailwindcss/forms'),
+  //   require('@tailwindcss/aspect-ratio'),
+  //   require('@tailwindcss/typography'),
+  //   require('tailwindcss-children'),
+  // ],
 }
 
